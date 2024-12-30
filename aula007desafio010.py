2b7dfd7308e21e8da05ea21242b862540a0bee1c
Fernando Veloso <fernando.veloso@vidibr.com>
2024-12-30 00:09:07 -0300

    Realizei o desafio 010 da aula 007.

--- aula007desafio010.py
+++ aula007desafio010.py
@@ -1,3 +1,15 @@
 # Crie um programa que leia quanto dinheiro a pessoa tem na carteira e mostre quantos dólares ela
 # pode comprar - considerar US$1.00 = R$5.00.
 
+# Jeito que eu fiz.
+real = float(input("Insira quantos reais você possui na carteira: R$"))
+dolar = 6.00
+print(f"O valor atual do dólar é de US${dolar:.2f} por dólar, sua carteira possui R${real:.2f} é "
+      f"possível comprar US${real/dolar:.3}")
+
+# Jeito que o prof Guanabara fez.
+real = float(input("Quanto dinheiro você tem na carteira? R$"))
+dolar = real / 6.00
+print("Com R${:.2f} você pode comprar US${:.2f}".format(real, dolar))
+
+
