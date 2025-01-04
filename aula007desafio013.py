b42c8a1ea8792511f859325d3667c719421482fc
Fernando Veloso <fernando.veloso@vidibr.com>
2025-01-03 23:06:09 -0300

    Comecei a fazer o desafio 013 da aula 007.

--- aula007desafio013.py
+++ aula007desafio013.py
@@ -1,2 +1,8 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de
 # aumento.
+
+salario_antigo = float(input("Insira seu salário: R$"))
+percentual_aumento = 15 # 15% de aumento salarial.
+novo_salario = salario_antigo * percentual_aumento / 100
+print(f"O seu salário anterior de R${salario_antigo:.2f} passa ser de R${novo_salario} após o "
+      f"reajuste de 15%")
