db1b822c4d41ac0ed394f269dc3c52c9a56f90c4
Fernando Veloso <fernando.veloso@vidibr.com>
2025-01-03 22:19:12 -0300

    Comecei a fazer o desafio 012 da aula 007.

--- aula007desafio012.py
+++ aula007desafio012.py
@@ -1,2 +1,6 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
+valor = float(input("Insira o valor o valor do produto: R$"))
+desconto = valor - (valor * 0.5)
+print(f"O valor do produto desejado é de R${valor}, com o desconto de 5% o novo valor do produto "
+      f"é de R${desconto}")
