a7becc7312cf189a2c63b34ef4421d7e7468aeda
Fernando Veloso <fernando.veloso@vidibr.com>
2025-01-05 06:42:56 -0300

    Adição do exercício 013 da aula 007.

--- aula007desafio012.py
+++ aula007desafio012.py
@@ -1,6 +1,17 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
-valor = float(input("Insira o valor o valor do produto: R$"))
-desconto = valor - (valor * 0.5)
-print(f"O valor do produto desejado é de R${valor}, com o desconto de 5% o novo valor do produto "
-      f"é de R${desconto}")
+# Jeito que eu fiz.
+
+valor = float(input("Insira o valor o valor do produto: R$")) # Solicito o valor do produto.
+percentual_desconto = 5 # 5% é o valor da percentual
+desconto = valor - (valor * percentual_desconto / 100) # Operação per centual
+print(f"O valor do produto desejado é de R${valor:.2f}, com o desconto de 5% o novo valor do "
+      f"produto "
+      f"é de R${desconto:.2f}")
+
+# Jeito que o prof Guanabra fez
+
+preco = float(input("Qual é o preço do seu produto? R$"))
+novo = preco - (preco * 5 / 100)
+print("O produto que custava R${}, na promoção com desconto de 5% vai custar R${}".format(preco,
+                                                                                          novo))
--- aula007desafio013.py
+++ aula007desafio013.py
@@ -1,8 +1,17 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de
 # aumento.
 
-salario_antigo = float(input("Insira seu salário: R$"))
-percentual_aumento = 15 # 15% de aumento salarial.
-novo_salario = salario_antigo * percentual_aumento / 100
-print(f"O seu salário anterior de R${salario_antigo:.2f} passa ser de R${novo_salario} após o "
-      f"reajuste de 15%")
+s = float(input("Insira o seu salário base: R$")) # s = Salário.
+a = 15 # a = aumento / 15 = 15%.
+
+# Decidi montar uma variável que já faça o cálculo de quanto seriam os 15% + o salário, primeiro
+# realizo a operação para descobrir o valor do reajuste, de acordo com o salário, e depois somo
+# esse valor a ele.
+
+ns = s + (s * a / 100) # ns = Novo Salário.
+# print(ns) = testei o resultado de ns para saber se estava gerando o aumento de 15% direto no
+# salário.
+
+# Faço um print mostrando para a pessoa o salário que ela digitou + o novo salário com o reajuste.
+print(f"O seu salário atual de R${s:.2f} com o reajuste de 15% passa a ser: R${ns:.2f}. Faça bom "
+      f"proveito do seu aumento!")
