099829935797ae0b6e7fc5e808f2419a593a6c60
Fernando Veloso <fernando.veloso@vidibr.com>
2025-01-07 02:38:26 -0300

    Fiz o desafio 016 da aula 008.

--- aula008desafio016.py
+++ aula008desafio016.py
@@ -2,4 +2,10 @@
 # inteira. Ex: Digite um número: 6.127. O n° 6.127 tem a parte inteira 6. Olhar funções da
 # biblioteca MATH.
 
+import math
+n = float(input("Insira um número com diversos dígitos: "))
+n_inteiro = math.floor(n)
+print(f"O seu número arredondado é: {n_inteiro}")
 
+# Esse exercício poderia ser feito com a função int, mas o professor pediu que fosse feito com a
+# biblioteca.
