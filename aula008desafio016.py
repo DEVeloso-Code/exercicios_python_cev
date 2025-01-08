21cdab9e42c42f0fa44092fa3b39f3911a7d300e
DEVeloso-Code <fernandovelosomkt@gmail.com>
2025-01-08 00:14:56 -0300

    Avaliação do desafio 016 da aula 008, inserção de novas versões e maneiras de se responder.

--- aula008desafio016.py
+++ aula008desafio016.py
@@ -3,9 +3,26 @@
 # biblioteca MATH.
 
 import math
-n = float(input("Insira um número com diversos dígitos: "))
-n_inteiro = math.floor(n)
-print(f"O seu número arredondado é: {n_inteiro}")
+n = float(input("Insira um número: "))
+n_inteiro = math.floor(n) # Floor sempre arredonda para baixo.
+print(f"O seu número inteiro é: {n_inteiro}")
 
 # Esse exercício poderia ser feito com a função int, mas o professor pediu que fosse feito com a
 # biblioteca.
+
+# Jeito que o prof Guanabara fez.
+import math
+num = float(input("Digite um valor: "))
+print("O valor digitado foi {} e a sua porção inteira é {}".format(num,math.trunc((num))))
+
+# Jeito que eu fiz para otimizar o código.
+from math import trunc # Importo apenas a funcionalidade dentro do Python.
+nm = float(input("Digite um valor: "))
+print(f"O valor digitado foi {nm} e sua porção inteira é {trunc(nm)}.")
+
+# 2 jeito que o prof Guanabara fez, sem importar nada
+numero = float(input("Digite um valor: "))
+print("O valor digitado foi {} e a sua porção inteira é {}".format(numero, int(numero))) #
+# Formatando o n° inteiro não existe a necessidade de se importar uma biblioteca.
+
+
