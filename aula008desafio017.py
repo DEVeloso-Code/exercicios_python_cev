9c884eef34d3b41631ba827a3c16d17cf49c233a
DEVeloso-Code <fernandovelosomkt@gmail.com>
2025-01-08 22:07:41 -0300

    Realização dos exercícios 017 e 018.

--- aula008desafio017.py
+++ aula008desafio017.py
@@ -1,3 +1,15 @@
 # Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo,
 # calcule e mostre o comprimento da hipotenusa.
 
+# Primeira solução:
+co = float(input("Comprimento do cateto oposto: "))
+ca = float(input("Comprimento do cateto adjacente: "))
+hi = (co ** 2 + ca ** 2) ** (1/2)
+print(f"A hipotenusa vai medir {hi:.2f}")
+
+# Segunda solução - Importando a biblioteca MATH.
+import math
+cop = float(input("Comprimento do cateto oposto: "))
+cad = float(input("Comprimento do cateto adjacente: "))
+hi = math.hypot(cop, cad)
+print(f"A hipotenusa vai medir {hi:.2f}")
--- aula008desafio018.py
+++ aula008desafio018.py
@@ -1,3 +1,14 @@
 # Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e
 # tangente desse ângulo.
 
+import math
+angulo = float(input("Digite o ângulo que você deseja: "))
+seno = math.sin(math.radians(angulo)) # Pega o ângulo digitado e converte para radianos.
+print(f"O ângulo de {angulo} tem o SENO de {seno:.2f}")
+cosseno = math.cos(math.radians(angulo)) # Pega o cosseno e converte para radians.
+print(f"O ângulo de {angulo} tem o COSSENO de {cosseno:.2f}")
+tangente = math.tan(math.radians(angulo)) # Pega a tangente e converte para radians.
+print(f"O ângulo de {angulo} tem a TANGENTE de {tangente:.2f}")
+
+# Poderia ser importado apenas as funcionalidades dentro da biblioteca.
+# from math import radians, sin, cos, tang
