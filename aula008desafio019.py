3228956cb62fb4e2552c7860d62504fca2a8ce0b
DEVeloso-Code <fernandovelosomkt@gmail.com>
2025-01-10 03:16:33 -0300

    Realização dos exercícios 019, 020 e 021 da aula 008, além do arquivo mp3 utilizado no exercício 021.

--- aula008desafio019.py
+++ aula008desafio019.py
@@ -1,2 +1,12 @@
 # Um professor quer sortear um dos seus 4 alunos para apagar o quadro. Faça um programa que ajude
 # ele, lendo o nome deles e escrevendo o nome do escolhido. Biblioteca RANDOM.
+
+import random # Importa o randomizador de elementos.
+# from random import choice - Faz a importação apenas da funcionalidade choice.
+n1 = str(input("Primeiro aluno: "))
+n2 = str(input("Segundo aluno: "))
+n3 = str(input("Terceiro aluno: "))
+n4 = str(input("Quarto aluno: "))
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista) # Faz uma escolha no item escolhido, nesse caso a lista.
+print(f"O aluno escolhido foi {escolhido}")
--- aula008desafio021.py
+++ aula008desafio021.py
@@ -1,2 +1,18 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 
+import pygame
+
+# Inicializar o mixer do pygame
+pygame.init()
+
+# Carregar o arquivo MP3
+pygame.mixer.music.load("aula008desafio021arquivo.mp3")
+
+# Tocar o arquivo MP3
+pygame.mixer.music.play()
+
+# Manter o programa rodando até o fim do áudio
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
+
