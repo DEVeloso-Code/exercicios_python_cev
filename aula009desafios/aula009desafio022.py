c74c819ad8d1099a34815c2317ac1a1e6dbf61d8
DEVeloso-Code <fernandovelosomkt@gmail.com>
2025-01-16 00:40:31 -0300

    Criação dos desafios da aula 009, focada em manipulação de strings.

---
+++ aula009desafios/aula009desafio022.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# 1) O nome com todas as letras maiúsculas.
+# 2) O nome com todas as letras minúsculas.
+# 3) Quantas letras ao possui (sem considerar espaços).
+# 4) Quantas letras tem o primeiro nome.
+
---
+++ aula009desafios/aula009desafio027.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o
+# último nome de separadamente.
+# Ex.: Ana Maria de Souza.
+# Primeiro = Ana.
+# Último = Souza.
+
+
