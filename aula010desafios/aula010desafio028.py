b9aad24620558b5412adc57475c6babc1764c800
DEVeloso-Code <fernandovelosomkt@gmail.com>
2025-01-28 02:32:46 -0300

    Exercícios 028 e 029 da aula 010 realizados.

--- aula010desafios/aula010desafio028.py
+++ aula010desafios/aula010desafio028.py
@@ -2,3 +2,15 @@
 #  tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na
 #  tela se o usuário venceu ou perdeu.
 
+# Importa a biblioteca random
+import random
+
+# Determina um intervalo de 1 a 5 onde o computador escolhe um número.
+numero_secreto = random.randint(1, 5)
+
+numero = int(input("Escolha um número de 1 a 5: "))
+
+if numero == numero_secreto:
+    print("Parabéns, você acertou!")
+else:
+    print(f"Você errou! O número secreto era {numero_secreto}")
--- aula010desafios/aula010desafio029.py
+++ aula010desafios/aula010desafio029.py
@@ -2,3 +2,16 @@
 # mensagem dizendo que ele foi multado.
 # A multa vai custar R$7.00 por cada km acima do limite.
 
+# Coleta a velocidade do veículo
+velocidade = int(input("Qual a velocidade do carro: "))
+# Expressa se a velocidade do veículo for maior que 80 deverá ser aplicado multa * 7
+if velocidade > 80:
+    multa = velocidade * 7
+    print(f"Seu carro estava a {velocidade}Km/h e por isso será multado em R${multa:.2f}.")
+else:
+    print(f"Seu carro estava a {velocidade}Km/h e por isso não será multado.")
+
+
+
+
+
