79da723032ea651d13f757d6c7e0299beebcd627
DEVeloso-Code <fernandovelosomkt@gmail.com>
2025-01-28 16:44:11 -0300

    Exercícios 030, 031 e 032 da aula 010 realizados.

--- aula010desafios/aula010desafio030.py
+++ aula010desafios/aula010desafio030.py
@@ -1,3 +1,10 @@
 #  Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
 
+# Solicitar um número qualquer.
+numero = int(input("Insira aqui seu número: "))
 
+# Definir se o número é par ou ímpar.
+if numero % 2 == 0:
+    print(f"O número {numero} é PAR!")
+else:
+    print(f"O número {numero} é ÍMPAR!")
--- aula010desafios/aula010desafio031.py
+++ aula010desafios/aula010desafio031.py
@@ -1,5 +1,13 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em KM. Calcule o preço da
 # passagem, cobrando R$0.50 por KM para viagens de até 200Km e R$0.45 para viagens mais longas.
 
+# Coleta a distância percorrida de uma viagem
+distancia = int(input("Qual será a distância em Km percorrida por você nesta viagem? "))
+if distancia <= 200:
+    passagem = distancia * 0.50
+    print(f"Sua passagem custará R${passagem:.2f}.")
+else:
+    passagem2 = distancia * 0.45
+    print(f"Sua passagem custará R${passagem2:.2f}.")
 
 
