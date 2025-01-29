49c493570dc211a6a579530e72d446ebbdf0757c
DEVeloso-Code <fernandovelosomkt@gmail.com>
2025-01-28 23:33:03 -0300

    Exercícios 033, 034 e 035 da aula 010 realizados.

--- aula010desafios/aula010desafio033.py
+++ aula010desafios/aula010desafio033.py
@@ -1,2 +1,13 @@
 # Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 
+n1 = int(input("Primeiro número: "))
+n2 = int(input("Segundo número: "))
+n3 = int(input("Terceiro número: "))
+
+if n1 < n2 and n1 < n3:
+    menor = n1
+elif n2 < n1 and n2 < n3:
+    menor = n2
+else:
+    menor = n3
+print(f"O menor número é {menor}.")
--- aula010desafios/aula010desafio034.py
+++ aula010desafios/aula010desafio034.py
@@ -2,3 +2,10 @@
 # Para salários superiores a R$1.500.00 calcule um aumento de 10%
 # Para os inferiores ou iguais, o aumento é de 15%.
 
+salario = float(input("Insira seu salário: "))
+if salario >= 1500:
+    aumento = salario * 1.10
+    print(f"Seu novo salário será de R${aumento:.2f}")
+else:
+    aumento2 = salario * 1.15
+    print(f"Seu novo salário será de R${aumento2:.2f}")
